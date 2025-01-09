7d41357a97056588e1cb3d2e4997838af69fd1a4
Anand Radha Krishnan <anandradhakrishna@outlook.com>
2025-01-09 11:06:17 +0100

    server side rendering

--- src/server/config.ts
+++ src/server/config.ts
@@ -1,8 +1,9 @@
 const env = process.env;
 
 export const PORT = env.PORT ?? "3002";
-export const HOST = env.HOST ?? "localhost";
+export const HOST = env.HOST ?? "0.0.0.0";
 export const SERVER_URL = `http://${HOST}:${PORT}`;
+export const API_SERVER_URL = `http://${HOST}:${PORT}/api`;
 
 export const MONGODB_URI =
   env.MONGODB_URL ?? "mongodb://localhost:27017";
---
+++ src/server/render.tsx
@@ -0,0 +1,20 @@
+import ReactDOMServer from "react-dom/server";
+
+import { fetchRecipe, fetchRecipes } from "../api-client";
+import App from "../components/App";
+
+const serverRender = async (request) => {
+  const { recipeId } = request.params;
+  console.log(request.params);
+
+  const initialData = recipeId
+    ? { currentRecipe: await fetchRecipe(recipeId) }
+    : { recipes: await fetchRecipes() };
+
+  const initialMarkup = ReactDOMServer.renderToString(
+    <App initialData={initialData} />,
+  );
+  return { initialMarkup, initialData };
+};
+
+export default serverRender;
